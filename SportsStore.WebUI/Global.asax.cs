64829bcf598c841ea63f68eb7d8a7b1df29ecd95
Jing <v-jzingh@microsoft.com>
2016-06-17 17:53:19 +0800

    Second time DB

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SportsStore.WebUI.Infrastructure;
 
 namespace SportsStore.WebUI
 {
-    public class MvcApplication : System.Web.HttpApplication
+    
+        public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +14,9 @@ namespace SportsStore.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new
+                NinjectControllerFactory());
         }
     }
 }
