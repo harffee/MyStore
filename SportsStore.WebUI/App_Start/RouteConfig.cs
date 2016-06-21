5659b3f8de35d456f442861897bc32369d312c2b
Jing <v-jzingh@microsoft.com>
2016-06-21 14:37:37 +0800

    css something

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SportsStore.WebUI
@@ -18,6 +14,11 @@ namespace SportsStore.WebUI
                 url:"Page{page}",
                 defaults: new { controller = "Product", action = "List"}
             );
+            routes.MapRoute(
+                name: "Default",
+                url: "{Controller}/{action}/{id}",
+                defaults: new { controller = "Product", action = "List", id = "UrlParameter.Optional" }
+                );
         }
     }
 }
--- SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -8,6 +8,8 @@ namespace SportsStore.WebUI.Models
         {
             public IEnumerable<Product> Products { get; set; }
             public PagingInfo PagingInfo { get; set; }
+
+            public string CurrentCategory { get ; set; }
         }
     
 }
