a74f75f3e3c5f3ac4388986caf6ff7af94d1e33e
Jing <v-jzingh@microsoft.com>
2016-06-28 10:33:25 +0800

    Add a shopping cart

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace SportsStore.WebUI.Controllers
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
-                .Where(p=>category==null||p.Category==category)
+                .Where(p => category == null || p.Category == category)
                 .OrderBy(p => p.ProductID)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize),
@@ -31,7 +31,9 @@ namespace SportsStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ?
+                    repository.Products.Count():
+                    repository.Products.Where(e=>e.Category==category).Count()
                 },
                 CurrentCategory = category
             };
